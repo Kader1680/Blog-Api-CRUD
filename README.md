2aee67f86e0719c559fec2fb2c91f643e18ffc53
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-12-01 19:17:45 +0100

    add delete all blog option

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fisrt-laravel-app
+<h2>Screen Shot About the Project</h2>
+<img src= "./public/pics/1.png" />
--- resources/views/editBlog.blade.php
+++ resources/views/editBlog.blade.php
@@ -1,17 +1,18 @@
 @extends("layout.master")
 
 @section("content")
-     <div class=" container">
-        <h2>Edit blog select </h2>
+     <div class="about container">
+        <h2 class=" text-center">Edit blog select </h2>
         <form action="{{route('update', ['post' => $post])}}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('put')
-            <label>Title</label>
-            <input value={{ $post->title }} style="width:100%; outline:none" name="title" type="text"  /><br><br>
-            <label>Body</label>
-            <input value={{ $post->body }} style="width:100%; height:12rem; outline:none" name="body"></textarea><br><br>
+            <label>New Title</label>
+            <input class=" p-3" style="width:100%; outline:none; border: 2.5px solid #ff993a" value={{ $post->title }} style="width:100%; outline:none" name="title" type="text"  /><br><br>
+            <label>New Body</label>
+            <textarea class=" p-3" style="width:100%; outline:none; border: 2.5px solid #ff993a"  value={{ $post->body }} style="width:100%; height:12rem; outline:none" name="body"></textarea><br><br>
 
-            <input style="width:50%" name="submit" type="submit" placeholder="post" />
+            {{-- <input style="width:50%" name="submit" type="submit" placeholder="post" /> --}}
+            <button class="btnPost" name="submit" type="submit" >Update Now</button>
         </form>
      </div>
 @endsection
