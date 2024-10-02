23fe3a7c3e074070f67cb44cad71eaf4bfab7d03
Kader1680 <ouldhenniabaghdad@gmail.com>
2024-10-02 19:00:25 +0100

    add new unit test restful api

--- tests/Feature/BlogTest.php
+++ tests/Feature/BlogTest.php
@@ -14,10 +14,17 @@ class BlogTest extends TestCase
     public function test_get_blogs(): void
     {
         $response = $this->json('GET', 'api/blog');
-        $response->assertStatus(200); 
+        $response->assertStatus(200);
+        $response->assertJsonStructure(
+            [
+                "status",
+                "error",
+                "data"
+            ]
+        );
     }
 
-    
+
     // public function test_create_blog(){
 
     //     $data = [
@@ -33,57 +40,56 @@ class BlogTest extends TestCase
     // }
 
 
-    public function test_get_by_id_blog(){
+    public function test_get_by_id_blog()
+    {
         $response = $this->get('api/blog');
 
         $response->assertStatus(200);
 
         $idBlog = $response->json('data')[0]['id'];
-        
-        
-        $update = $this->json('GET', 'api/blog/'.$idBlog);
+
+
+        $update = $this->json('GET', 'api/blog/' . $idBlog);
 
         $update->assertStatus(200);
-    
     }
 
 
-
-    public function test_update_blog(){
+    public function test_update_blog()
+    {
         $response = $this->get('api/blog');
 
         $response->assertStatus(200);
 
         $idBlog = $response->json('data')[0]['id'];
-        
-        
-        $update = $this->json('PUT', 'api/edit/'.$idBlog, [
+
+
+        $update = $this->json('PUT', 'api/edit/' . $idBlog, [
             'title' => "e",
             'body' => 'wouw'
         ]);
 
         $update->assertStatus(200);
-    
     }
 
 
-    public function test_create_blog(){
-        
-        
+    public function test_create_blog()
+    {
+
+
         $newBlog = $this->json('POST', 'api/create/', [
             'title' => "new blog ",
             'body' => 'new body'
         ]);
 
         $newBlog->assertStatus(201);
-    
     }
 
-    public function test_delete_blog(){
+    public function test_delete_blog()
+    {
         $response = $this->get('/api/blog');
-        $idBlog = $response->json('data')[0]['id'];
-        $newBlog = $this->json('DELETE', 'api/delete/'.$idBlog);
+        $idBlog = $response->json('data')[3]['id'];
+        $newBlog = $this->json('DELETE', 'api/delete/' . $idBlog);
         $newBlog->assertStatus(204);
-    
     }
 }
